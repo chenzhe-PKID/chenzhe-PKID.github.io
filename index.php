a3d27d68c0a8d23bc2ac8264804127a46b35858e
chenzhe-PKID <2127950295@qq.com>
2020-01-12 16:55:47 +0800

    ..

--- index.php
+++ index.php
@@ -1,54 +1,17 @@
 <?php
 /**
- * The main template file.
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link    https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Shapely
+ * @package WordPress
  */
-get_header(); ?>
-<?php $layout_class = ( function_exists( 'shapely_get_layout_class' ) ) ? shapely_get_layout_class() : ''; ?>
-	<div class="row">
-		<?php
-		if ( $layout_class == 'sidebar-left' ):
-			get_sidebar();
-		endif;
-		?>
-		<div id="primary" class="col-md-8 mb-xs-24 <?php echo esc_attr( $layout_class ); ?>"><?php
-			if ( have_posts() ) :
 
-				if ( is_home() && ! is_front_page() ) : ?>
-					<header>
-						<h1 class="page-title screen-reader-text"><?php esc_html( single_post_title() ); ?></h1>
-					</header>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-					<?php
-				endif;
-
-				$layout_type = get_theme_mod( 'blog_layout_view', 'grid' );
-
-				get_template_part( 'template-parts/layouts/blog', $layout_type );
-
-				if ( function_exists( "shapely_pagination" ) ):
-					shapely_pagination();
-				endif;
-
-			else :
-
-				get_template_part( 'template-parts/content', 'none' );
-
-			endif; ?>
-		</div><!-- #primary -->
-		<?php
-		if ( $layout_class == 'sidebar-right' ):
-			get_sidebar();
-		endif;
-		?>
-	</div>
-<?php
-get_footer();
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
